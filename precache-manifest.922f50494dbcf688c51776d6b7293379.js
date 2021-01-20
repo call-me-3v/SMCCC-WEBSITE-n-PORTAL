8e24fb0b079e11cc5cdca8b4fcd5cd0e18ca48f7
Vincent Asamoah <3vdataconsult@gmail.com>
2021-01-20 21:12:23 +0000

    Updates

--- precache-manifest.922f50494dbcf688c51776d6b7293379.js
+++ precache-manifest.922f50494dbcf688c51776d6b7293379.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "06164c730aaf4d263067a3df6bd78f5c",
+    "revision": "4f6a82bf7c0f7b1bb9870b47d07e5c6e",
     "url": "/SMCCC-WEBSITE-n-PORTAL/index.html"
   },
   {
-    "revision": "6a016c3d53f2667a76b0",
-    "url": "/SMCCC-WEBSITE-n-PORTAL/static/css/main.2d244a27.chunk.css"
+    "revision": "f2b56d9e7a70262014ee",
+    "url": "/SMCCC-WEBSITE-n-PORTAL/static/css/main.4511b559.chunk.css"
   },
   {
-    "revision": "f969a7e53fe6d88314d6",
-    "url": "/SMCCC-WEBSITE-n-PORTAL/static/js/2.aef7eba6.chunk.js"
+    "revision": "90a0189e031c8aa00357",
+    "url": "/SMCCC-WEBSITE-n-PORTAL/static/js/2.bb0bfdd5.chunk.js"
   },
   {
-    "revision": "8090db5edb120f0e11cf6f60f5c03298",
-    "url": "/SMCCC-WEBSITE-n-PORTAL/static/js/2.aef7eba6.chunk.js.LICENSE.txt"
+    "revision": "5d8ed9bc3aa5af1a7ba683d956090c41",
+    "url": "/SMCCC-WEBSITE-n-PORTAL/static/js/2.bb0bfdd5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6a016c3d53f2667a76b0",
-    "url": "/SMCCC-WEBSITE-n-PORTAL/static/js/main.55dd4008.chunk.js"
+    "revision": "f2b56d9e7a70262014ee",
+    "url": "/SMCCC-WEBSITE-n-PORTAL/static/js/main.321f2564.chunk.js"
   },
   {
     "revision": "dcf20057566dbbbc379d",
@@ -36,11 +36,19 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/SMCCC-WEBSITE-n-PORTAL/static/media/bg7.a3910eff.jpg"
   },
   {
+    "revision": "fafedcd635de7f12fe042703d298b79c",
+    "url": "/SMCCC-WEBSITE-n-PORTAL/static/media/groups-placeholder.fafedcd6.png"
+  },
+  {
     "revision": "9ac0ca860a17035025c6828e690d68fc",
     "url": "/SMCCC-WEBSITE-n-PORTAL/static/media/loupe.9ac0ca86.svg"
   },
   {
     "revision": "0d53f7233cc6a3b3d4680d077dce6c73",
     "url": "/SMCCC-WEBSITE-n-PORTAL/static/media/stmic.0d53f723.png"
+  },
+  {
+    "revision": "c9cd6b974c53a7bb84604ecbeb30e425",
+    "url": "/SMCCC-WEBSITE-n-PORTAL/static/media/youthchoir.c9cd6b97.jpg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SMCCC-WEBSITE-n-PORTAL/precache-manifest.30f984b367d34ba81e33691126d79665.js"
+  "/SMCCC-WEBSITE-n-PORTAL/precache-manifest.922f50494dbcf688c51776d6b7293379.js"
 );
 
 self.addEventListener('message', (event) => {
